11c0988a2e8c686cc6dd01e9a778c17293e6ef8d
leeseojune <sung07288346@gmail.com>
2021-01-28 23:10:55 +0900

    UPDATE; routes go file

--- ginjwt/routes/routes.go
+++ ginjwt/routes/routes.go
@@ -1,1 +1,19 @@
 package routes
+
+import(
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"ginjwt/controllers"
+)
+
+func SetupRoutes(db *gorm.DB) *gin.Engine {
+	r := gin.Default()
+	r.Use(func(c *gin.Context) {
+		c.Set("db", db)
+	})
+
+	r.POST("/auth", controllers.Login)
+	r.POST("/register", controllers.CreateUser)
+
+	return r
+}
