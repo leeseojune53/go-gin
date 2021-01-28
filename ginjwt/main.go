1559786996d61b7cf6d2814f66f3f63f41a6998e
leeseojune <sung07288346@gmail.com>
2021-01-28 23:10:40 +0900

    UPDATE; main go file

--- ginjwt/main.go
+++ ginjwt/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main()  {
+import (
+	"ginjwt/models"
+	"ginjwt/routes"
+)
 
+func main()  {
+	db := models.SetupDB()
+	db.AutoMigrate(&models.User{})
+
+	r := routes.SetupRoutes(db)
+
+	r.Run()
 }
