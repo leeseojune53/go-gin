72a302236afffef544aa1533b6f1ebd2cdf093eb
leeseojune <sung07288346@gmail.com>
2021-01-28 23:10:22 +0900

    UPDATE; jwt controller

--- ginjwt/controllers/jwt.go
+++ ginjwt/controllers/jwt.go
@@ -1,16 +1,18 @@
 package controllers
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"ginjwt/models"
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
-	jwt "github.com/appleboy/gin-jwt/v2"
 	"net/http"
 	"os"
 	"time"
 )
 
-type CreateUser struct {
+type User struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
@@ -18,6 +20,33 @@ type CreateUser struct {
 type LoginUser struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+func CreateUser(c *gin.Context)  {
+	var input User
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, "Invalid json provided")
+		return
+	}
+
+	user := models.User{Username: input.Username, Password: hashing(input.Password)}
+
+	db := c.MustGet("db").(*gorm.DB)
+	db.Create(&user)
+
+	c.JSON(http.StatusOK, "Create user Success")
+}
+
+func hashing(data string) string {
+	hash := sha256.New()
+
+	hash.Write([]byte(data))
+
+	md := hash.Sum(nil)
+	mdStr := hex.EncodeToString(md)
+
+	return mdStr
 }
 
 func Login(c *gin.Context)  {
@@ -40,20 +69,33 @@ func Login(c *gin.Context)  {
 		return
 	}
 
-	c
+	token, err := CreateToken(user.ID)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, token)
+
 }
 
 func CreateToken(userid uint64) (string, error) {
 	var err error
 
 	os.Setenv("ACCESS_SECRET", "testAccessKey")
+
 	atClaims := jwt.MapClaims{}
-	atClaims["authorized"] = true;
-	atClaims["user_id"] = userid;
+	atClaims["authorized"] = true
+	atClaims["user_id"] = userid
 	atClaims["exp"] = time.Now().Add(time.Hour * 2).Unix()
-	at := jwt.New(&jwt.GinJWTMiddleware{
-		PayloadFunc: func(data interface{}) jwt.MapClaims {
-			return atClaims{}
-		},
-	})
+
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+
+	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+
+	if err != nil {
+		return "", err
+	}
+	return token, nil
 }
